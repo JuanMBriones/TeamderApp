fc483021acee60e451e50255e59dc9242514605a
JuanMa <JuanMBriones@outlook.com>
2020-11-14 19:21:44 -0600

    Fixed bug at ion-menu selector
    Fixed fug ar menu selector on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,39 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Teams',
+      url: '/folder/Teams',
+      icon: 'people'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Match',
+      url: '/folder/Match',
+      icon: 'flame'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      title: 'Meetings',
+      url: '/folder/Meetings',
+      icon: 'calendar'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public userPages = [
+    {
+      title: 'Profile',
+      url: '/folder/Profile',
+      icon: 'person-circle'
+    },
+    {
+      title: 'Login',
+      url: '/folder/Login',
+      icon: 'finger-print'
+    },
+    {
+      title: 'Logout',
+      url: '/folder/Logout',
+      icon: 'finger-print'
+    }
+  ];
+  //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
